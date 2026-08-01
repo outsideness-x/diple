2b72ebd07c5a9827c977b94d7872b73d526d4629
outsideness-x <kravchenkoalex02@gmail.com>
2026-08-01 11:33:34 +0300

    Initial Commit

---
+++ diple/dipleApp.swift
@@ -0,0 +1,17 @@
+//
+//  dipleApp.swift
+//  diple
+//
+//  Created by chemical_pink on 01.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct dipleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
